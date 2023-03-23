5a3ecd67a5bfbc51aad7fa3c990f00f2c0e4dde0
aandonovbg <aandonovbg@gmail.com>
2023-03-23 10:31:48 +0200

    MyChartScript.js edited index.html edited

--- src/main/resources/static/own/scripts/MyChartScript.js
+++ src/main/resources/static/own/scripts/MyChartScript.js
@@ -1,4 +1,5 @@
-fetch('/client/allClientsData')
+
+  fetch('/client/allClientsData')
     .then(response => response.json())
     .then(data => {
         const clientNames = [];
@@ -13,20 +14,33 @@ fetch('/client/allClientsData')
             data: {
                 labels: clientNames,
                 datasets: [{
-                    label: 'Number of Projects',
+                    label: 'Брой Проекти',
                     data: numProjects,
                     backgroundColor: 'rgba(54, 162, 235, 0.2)',
                     borderColor: 'rgba(54, 162, 235, 1)',
-                    borderWidth: 1
+                    borderWidth: 1,
+                    borderRadius: 5, // adjust this value to change the radius of the rounded corners
+                    barThickness: 40 // adjust this value to change the bar width
                 }]
             },
             options: {
                 scales: {
                     yAxes: [{
                         ticks: {
-                            beginAtZero: true
+                            beginAtZero: true,
+                            stepSize: 1,
+                callback: function(value, index, values) {
+                  if (Math.floor(value) === value) {
+                    return value;
+                  }
+                }
                         }
                     }]
+                },
+                plugins: {
+                    ovalBars: {
+                        ratio: 0.5 // adjust this value to change the oval shape
+                    }
                 }
             }
         });
